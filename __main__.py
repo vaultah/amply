29cd6b253eff77c0d0730f7509ee7c082a23a752
Riley Banks <waultah@gmail.com>
2016-11-29 16:53:44 +0000

    WIP: read the config file inside the package. Not tested.

--- __main__.py
+++ __main__.py
@@ -1,94 +1,65 @@
-import shutil
-import errno
 import argparse
+import errno
+import logging
+import pkgutil
 from filecmp import dircmp
 from pathlib import Path
 from json import JSONDecoder
 from collections import OrderedDict
-from abc import ABC, abstractmethod
+from tasks import CopyTask, RemovalTask
 
 decoder = JSONDecoder(object_pairs_hook=OrderedDict)
 
-
-class Task(ABC):
-
-    def __init__(self, source, target):
-        self.source, self.target = source, target
-
-    @classmethod
-    def _recursive_cmp(cls, dir, cmp):
-        yield from (dir / x for x in cmp.left_only + cmp.diff_files)
-        for k, v in cmp.subdirs.items():
-            yield from cls._recursive_cmp(dir / k, v)
-
-    def diff(self):
-        cmp = dircmp(str(self.source), str(self.target))
-        yield from self._recursive_cmp(Path(), cmp)
-
-    @abstractmethod
-    def run(confirm=False):
-        pass
-
-
-class CopyTask(Task):
-
-    def run(self, confirm=False):
-        for path in self.diff():
-            src, dest = str(self.source / path), str(self.target / path)
-            if confirm and input('Copy {} to {}? '.format(src, dest)) not in 'yY':
-                continue
-            try:
-                shutil.copy(src, dest)
-            except OSError as e:
-                if e.errno == errno.EISDIR:
-                    shutil.copytree(src, dest)
-                else:
-                    raise e from None
-
-
-class RemovalTask(Task):
-
-    def run(self, confirm=False):
-        for path in self.diff():
-            path = self.source / path
-            if confirm and input('Remove {}?'.format(path)) not in 'yY':
-                continue
-            try:
-                path.unlink()
-            except OSError as e:
-                if e.errno == errno.EISDIR:
-                    shutil.rmtree(str(path))
-                else:
-                    raise e from None
-
+# TODO: better help message
 
 parser = argparse.ArgumentParser()
-parser.add_argument('source')
+parser.add_argument('source', nargs='?')
 parser.add_argument('targets', nargs='*')
 parser.add_argument('--confirm', '-c', action='store_true')
 parser.add_argument('--list', '-l',  action='store_true')
 parser.add_argument('--remove-extra', '-r', action='store_true')
+parser.add_argument('--log-level', default='warning')
 args = parser.parse_args()
-
-source = Path(args.source).resolve()
-tasks = OrderedDict()
 
 if args.list and (args.confirm or args.remove_extra):
     raise RuntimeError('--list/-l must be sole argument')
 
-# Can be a path to the source directory or
-# a path to the configutation file
-if not source.is_dir():
-    config = decoder.decode(source.read_text())
-    for k, v in config.items():
-        tasks[k] = [CopyTask(v['source'], Path(t).resolve()) for t in v['targets']]
-else:
-    tasks[source.stem] = [CopyTask(source, Path(t).resolve()) for t in args.targets]
+logging.basicConfig(level=args.log_level.upper())
+
+# First, try to load 'config.json' from the current ZIP file. If that fails, use
+# the command line arguments. If the first argument isn't a directory, assume
+# that it points to the configutation file; otherwise treat it as the source
+# directory and the rest of positional arguments as target directories
+
+# Create tasks
+tasks = OrderedDict()
+
+try:
+    data = pkgutil.get_data(__name__, 'config.json')
+    if data is None:
+        raise OSError('The loader does not support get_data')
+except OSError as e:
+    logging.debug('pkgutil.get_data raised an exception: {}'.format(e))
+    logging.info('Loading configuration from the current package failed')
+    source = Path(args.source).resolve()
+    if not source.is_dir():
+        logging.info('The first argument isn\'t a directory')
+        data = source.read_bytes()
+    else:
+        logging.info('The first argument is the source directory, the rest are targets')
+        tasks[source.stem] = [CopyTask(source, Path(t).resolve()) for t in args.targets]
+        data = None
+finally:
+    if data is not None:
+        config = decoder.decode(data.decode())
+        for k, v in config.items():
+            tasks[k] = [CopyTask(v['source'], Path(t).resolve()) for t in v['targets']]
 
 if args.remove_extra:
     for k, v in tasks.items():
         v.extend([RemovalTask(t.target, t.source) for t in v])
 
+# Execute tasks
 for _, v in tasks.items():
     for task in v:
         if args.list:
